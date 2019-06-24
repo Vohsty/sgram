9a71e0864bce7a0308de989ea640d9fefc38fb1c
Vohsty <stevekimanthi87@gmail.com>
2019-06-24 09:47:32 +0300

    add app urls.py

---
+++ photos/urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls import url
+from django.conf.urls.static import static
+from django.conf import settings
+from . import views
+
+urlpatterns=[
+    url('^$', views.home, name = 'home'),
+    url(r'^upload$', views.upload, name='upload'),
+    url(r'^myprofile$',views.myprofile,name='myprofile'),
+    url(r'myprofile/edit$',views.edit,name='edit'),
+    url(r'profile/user/(\d+)/$',views.profile,name='users'),
+    url(r'image/comment/(\d+)/$',views.comments,name='comment'),
+    url(r"^search/",views.search,name="search")
+]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
